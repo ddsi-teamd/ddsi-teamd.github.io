40cfbf88404f9dfb5ef52ace29c2ce16767b1083
Ricardo Castro <rcastro@designdata.com>
2020-04-16 09:34:01 -0400

    updates

--- assets/components/content.js
+++ assets/components/content.js
@@ -10,7 +10,7 @@ var component_content=Vue.component('component-content', {
     <div class="content">
       <div class="card">
         <div class="card-body overflow-hidden p-lg-6">
-          
+          HI THERE!
           <div class="row" v-for="chunk in customers"> 
 
             <component-company v-for="customer in chunk" v-bind:customer="customer" v-bind:key="customer.id"></component-company>
